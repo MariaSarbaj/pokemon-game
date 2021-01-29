3956c049814526a8f95f9b5102bac1358318c85f
MariaSarbaj <marysarbaj@gmail.com>
2021-01-29 18:40:32 +0200

    Homework 3

---
+++ src/components/pokemon-card/pokemon-card.js
@@ -0,0 +1,53 @@
+import { useState} from 'react';
+
+import style from './pokemon-card.module.css';
+
+import cardBackSide from './assets/card-back-side.jpg';
+
+const PokemonCard = ({name, img, type, id, values}) => {
+    const [isActive, setActive] = useState(false);
+
+    const onCardClick = () => {
+        setActive(true);
+    }
+
+    return (
+        <div className={style.root} onClick={onCardClick}>
+            <div className={`${style.pokemonCard} ${isActive ? style.active : ''}`}>
+                <div className={style.cardFront}>
+                    <div className={`${style.wrap} ${style.front}`}>
+                        <div className={`${style.pokemon} ${style[type]}`}>
+                            <div className={style.values}>
+                                <div className={`${style.count} ${style.top}`}>{values.top}</div>
+                                <div className={`${style.count} ${style.right}`}>{values.right}</div>
+                                <div className={`${style.count} ${style.bottom}`}>{values.bottom}</div>
+                                <div className={`${style.count} ${style.left}`}>{values.left}</div>
+                            </div>
+                            <div className={style.imgContainer}>
+                                <img src={img} alt={name} />
+                            </div>
+                            <div className={style.info}>
+                                <span className={style.number}>#{id}</span>
+                                <h3 className={style.name}>
+                                    {name}
+                                </h3>
+                                <small className={style.type}>Type:
+                                    <span>{type}</span>
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={style.cardBack}>
+                    <div className={`${style.wrap} ${style.back}`}>
+                        <img src={cardBackSide} alt="Сard Backed" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default PokemonCard;
